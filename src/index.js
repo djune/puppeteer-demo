9a109a2fffd59debd2dd7c404fc2aa710c8c9141
djune <jiangcy@tuya.com>
2022-03-07 22:22:17 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const puppeteer = require('puppeteer');
 const init = async (str) => {
   const startTime = new Date().getTime()
   console.log('startTime', startTime)
+  
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.baidu.com');
   await page.screenshot({ path: 'example.png' });
   await browser.close();
+
   console.log('endTime', new Date().getTime() - startTime)
 }
 
